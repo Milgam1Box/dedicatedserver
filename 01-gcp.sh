b7ba33e864c307f444a66627aa4d3911b92345e0
KorTechTim <127585101+KorTechTim@users.noreply.github.com>
2023-03-14 10:12:16 +0900

    Create 01-gcp.sh

---
+++ 01-gcp.sh
@@ -0,0 +1,13 @@
+# 계정 생성 및 권한 설정
+echo "-----------------------------------------"
+echo "steam ID를 생성합니다. 계정의 비밀번호를 지정해주세요"
+echo "-----------------------------------------"
+echo 
+
+
+sudo useradd -m -d /home/steam steamcmd
+sudo passwd steamcmd
+sudo usermod -aG sudo steamcmd
+cd /home/steam
+sudo su steamcmd
+pwd
