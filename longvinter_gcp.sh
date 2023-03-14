917474ec02edaeb0089f77ca7b73eea2bd4a6a9e
KorTechTim <127585101+KorTechTim@users.noreply.github.com>
2023-03-14 09:57:24 +0900

    Update longvinter_gcp.sh

--- longvinter_gcp.sh
+++ longvinter_gcp.sh
@@ -2,6 +2,11 @@
 
 
 # 계정 생성 및 권한 설정
+echo "-----------------------------------------"
+echo "steam ID를 생성합니다. 계정의 비밀번호를 지정해주세요"
+echo "-----------------------------------------"
+echo 
+
 
 sudo useradd -m -d /home/steam steamcmd
 sudo passwd steamcmd
