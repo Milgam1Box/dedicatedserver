1b814336dd40afed1d45917771ea13ff326e99db
KorTechTim <127585101+KorTechTim@users.noreply.github.com>
2023-03-14 10:19:33 +0900

    Update 01-gcp.sh

--- 01-gcp.sh
+++ 01-gcp.sh
@@ -1,6 +1,7 @@
 # 계정 생성 및 권한 설정
 echo "-----------------------------------------"
 echo "steam ID를 생성합니다. 계정의 비밀번호를 지정해주세요"
+echo "계정 생성이 완료되고 나며 반드시 cd 커맨드를 한버 입력해주세요"
 echo "-----------------------------------------"
 echo 
 
@@ -8,14 +9,4 @@ sudo useradd -m -d /home/steam steamcmd
 sudo passwd steamcmd
 sudo usermod -aG sudo steamcmd
 sudo su steamcmd
-clear
-
-echo
-echo
-echo
-echo
-echo "-----------------------------------------"
-echo "steam ID를 생성이 완료 되었습니다. 
-echo "반듯 cd 커맨드를 한번 입력해 주세요.
-echo "-----------------------------------------"
 
