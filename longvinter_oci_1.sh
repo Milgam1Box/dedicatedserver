0279868b85ea4f52e4da22e6bfd0d038c47d981a
Tim <81416877+Milgam1Box@users.noreply.github.com>
2023-03-20 08:30:33 +0900

    Update longvinter_oci_1.sh

--- longvinter_oci_1.sh
+++ longvinter_oci_1.sh
@@ -1,6 +1,7 @@
 # 계정 생성 및 권한 설정
 echo "--------------------------------------------------------"
 echo "Tech Tim 채널 구독과 좋아요 부탁 드립니다."
+echo "https://www.youtube.com/channel/UCTKC1JQaVY-QvLKyGt84I5w"
 echo "steamcmd 계정으 생성합니다. 새 패스워드를 넣어 주세요"
 echo "--------------------------------------------------------"
 echo 
